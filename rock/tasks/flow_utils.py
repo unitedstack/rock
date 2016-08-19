2914edff6f54f9502e966dc24619332104b97ce9
liudong <willowd878@gmail.com>
2016-08-19 15:47:10 +0800

    Add skeleton of tasks

---
+++ rock/tasks/flow_utils.py
@@ -0,0 +1,77 @@
+# Copyright 2011 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+from oslo_log import log as logging
+from taskflow.listeners import base
+from taskflow.listeners import logging as logging_listener
+from taskflow import task
+
+LOG = logging.getLogger(__name__)
+
+
+def _make_task_name(cls):
+    """Makes a pretty name for a task class."""
+    base_name = ".".join([cls.__module__, cls.__name__])
+    return base_name
+
+
+class BaseTask(task.Task):
+    """The root task class for all tasks.
+
+    It automatically names the given task using the module and class that
+    implement the given task as the task name.
+    """
+
+    def __init__(self, **kwargs):
+        super(BaseTask, self).__init__(self.make_name(), **kwargs)
+
+    @classmethod
+    def make_name(cls):
+        return _make_task_name(cls)
+
+
+class DynamicLogListener(logging_listener.DynamicLoggingListener):
+    """This is used to attach to taskflow engines while they are running.
+
+    It provides a bunch of useful features that expose the actions happening
+    inside a taskflow engine, which can be useful for developers for debugging,
+    for operations folks for monitoring and tracking of the resource actions
+    and more...
+    """
+
+    #: Exception is an excepted case, don't include traceback in log if fails.
+    _NO_TRACE_EXCEPTIONS = ()
+
+    def __init__(self, engine,
+                 task_listen_for=base.DEFAULT_LISTEN_FOR,
+                 flow_listen_for=base.DEFAULT_LISTEN_FOR,
+                 retry_listen_for=base.DEFAULT_LISTEN_FOR,
+                 logger=LOG):
+        super(DynamicLogListener, self).__init__(
+            engine,
+            task_listen_for=task_listen_for,
+            flow_listen_for=flow_listen_for,
+            retry_listen_for=retry_listen_for,
+            log=logger)
+
+    def _format_failure(self, fail):
+        if fail.check(*self._NO_TRACE_EXCEPTIONS) is not None:
+            exc_info = None
+            exc_details = '%s%s' % (os.linesep, fail.pformat(traceback=False))
+            return (exc_info, exc_details)
+        else:
+            return super(DynamicLogListener, self)._format_failure(fail)
---
+++ rock/tasks/manager.py
@@ -0,0 +1,40 @@
+# Copyright 2011 OpenStack Foundation.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_log import log as logging
+import taskflow.engines
+from taskflow.patterns import linear_flow
+
+from rock.tasks import flow_utils
+
+LOG = logging.getLogger(__name__)
+
+
+def run_flow(flow_name, tasks):
+    """Constructs and run a task flow.
+    """
+
+    task_flow = linear_flow.Flow(flow_name)
+
+    task_flow.add(
+        tasks
+    )
+
+    # Now load (but do not run) the flow using the provided initial data.
+    flow_engine = taskflow.engines.load(task_flow)
+
+    with flow_utils.DynamicLogListener(flow_engine, logger=LOG):
+        flow_engine.run()
+        LOG.info("Volume created successfully.")
