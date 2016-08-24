7cb1c67ac44814c5a140ff70876774f68a9c0623
AndiaQ <1132458419@qq.com>
2016-08-24 09:44:02 +0000

    rm adapter.py

--- rock/tasks/test.py
+++
@@ -1,9 +0,0 @@
-import adapter
-
-if __name__ == '__main__':
-    c = adapter.CommonAdapter()
-    #c.evacuate_vm()
-    #c.list_services()
-    #import ptpdb; ptpdb.set_trace()
-    c.power_operation('10.0.108.119','root','PQ79ISF7ha7G')
-
