f4842904479b1f74089c5efddba01b63c75396fa
AndiaQ <1132458419@qq.com>
2016-08-24 02:32:23 +0000

    modified

--- rock/tasks/ipmi.py
+++ rock/tasks/ipmi.py
@@ -9,7 +9,6 @@ class IPMITool():
         self._password = password
  
     def power_on(self):
-<<<<<<< HEAD
         cmd = 'ipmitool -I lanplus -H ' +str(self._target) + ' -U ' \
               + str(self._username) + ' -P '+ str(self._password) +' chassis power on'
         os.system(cmd)
@@ -28,18 +27,6 @@ class IPMITool():
         cmd = 'ipmitool -I lanplus -H ' +str(self._target) + ' -U ' \
               + str(self._username) + ' -P '+ str(self._password) +' chassis power reset'
         os.system(cmd)
-=======
-        cmd = ipmitool -I lanplus -H self._target -U self._username -P self._password power on
-
-    def power_off(self):
-        cmd = ipmitool -I lanplus -H self._target -U self._username -P self._password power off 
-
-    def power_cycle(self):
-        cmd = ipmitool -I lanplus -H self._target -U self._username -P self._password power cycle
-
-    def power_reset(self):
-        cmd = ipmitool -I lanplus -H self._target -U self._username -P self._password power reset
->>>>>>> 16f9b8a4eeb66a08662cfce3e7fce66ae2c3b10d
 
     def power_status(self):
         cmd = 'ipmitool -I lanplus -H ' +str(self._target) + ' -U ' \
