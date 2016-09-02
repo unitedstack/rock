fd113c92e12af3ca3b464e91794374f049151433
AndiaQ <1132458419@qq.com>
2016-09-02 03:06:42 +0000

    Add some exception judgement

--- rock/tasks/host_evacuate.py
+++ rock/tasks/host_evacuate.py
@@ -14,24 +14,10 @@ class HostEvacuate(BaseTask,NovaAction,IPMIAction):
         host = target
         n_client = self._get_client()
 
-
-        LOG.info("checking the state of nova compute service on the host %s" % host)
-        services = n_client.services.list()
-        flag = False
-        while not flag:
-            for s in services:
-                if s.host == unicode(host) and s.binary == u'nova-compute' and s.state == u'down':
-                    flag = True
-                    print "<Service:%s, Host:%s, State:%s>" % (s.binary,s.host,s.state)
-                    break
-                else:
-                    flag = False
-                    time.sleep(5)
         evacuated_host = host
         evacuable_servers = n_client.servers.list(
             search_opts={'host':evacuated_host,
                          'all_tenants':1})
-
         evacuated_servers = list()
         for server in evacuable_servers:
             LOG.debug("Processing %s" % server)
@@ -40,13 +26,22 @@ class HostEvacuate(BaseTask,NovaAction,IPMIAction):
                 if response['accepted']:
                     LOG.info("Evacuated %s from %s: %s" %
                                  (response["uuid"], evacuated_host, response["reason"]))
-                    evacuated_servers.append(server)
+                    evacuated_servers.append(server.id)
                 else:
                     LOG.error("Evacuation of %s on %s failed: %s" %
                                   (response["uuid"], evacuated_host, response["reason"]))
             else:
                 LOG.error("Could not evacuate instance: %s" % server.to_dict())
+        
+        evacuated = list()
+        for ID in evacuated_servers:
+           evacuated.append(n_client.servers.get(ID))
+        for server in evacuated:
+            LOG.debug("checking the status of server: %s" % server.id)
+            server.host = getattr(server,'OS-EXT-SRV-ATTR:host')
+            if server.host != unicode(host):
+                LOG.info("Evacuate server %s successfully" % server.id)
+            else:
+                LOG.info("Evacuate server %s failed, its status is %s" % (server.id, server.status))
 
         
-
-
--- rock/tasks/power_manager.py
+++ rock/tasks/power_manager.py
@@ -2,6 +2,7 @@ from actions import IPMIAction
 from flow_utils import BaseTask
 from oslo_log import log as logging
 import time
+import json
 
 LOG = logging.getLogger(__name__)
 
@@ -12,11 +13,19 @@ class PowerManager(BaseTask,IPMIAction):
         try:
             info = IPMIAction(target)
             info.power_off()
+            time.sleep(5)
             result = info.power_status()
-            if result == 0:
+
+            if result == None:
                 LOG.info("Success to power off host %s" % target)
+                with open('/etc/rock/target.json','r+') as f:
+                    data = json.load(f)
+                    if data.get(target, None) is not None:
+                        data.pop(target)
+                with open('/etc/rock/target.json','w+') as f:
+                    json.dump(data,f)
+
             return True
         except Exception as e:
             return False
-
 
